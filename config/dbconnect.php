9367a3ca9c7015f6506c3272a4c86dcf49f071ef
MONATITY <namanemonatity@gmail.com>
2025-06-22 21:29:19 +0000

    ajout des modifie

--- config/dbconnect.php
+++ config/dbconnect.php
@@ -2,8 +2,8 @@
 class Database {
     private $host = "localhost";
     private $db_name = "etatcivil";
-    private $username = "groupe_pct";
-    private $password = "groupe27";
+    private $username = "root";
+    private $password = "";
     public $conn;
 
     public function getConnection() {
